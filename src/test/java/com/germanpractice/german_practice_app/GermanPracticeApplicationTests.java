550aac0f710e817c5035e2870abc910aaf685582
ddkatona <ddkatona@gmail.com>
2024-07-11 21:17:08 +0200

    Initial commit

---
+++ src/test/java/com/germanpractice/german_practice_app/GermanPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.germanpractice.german_practice_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GermanPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
